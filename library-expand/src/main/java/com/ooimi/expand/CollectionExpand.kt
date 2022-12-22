eb79c3b397c5289ac10c5c2868354206940d441a
尹帅 <1317972280@qq.com>
2022-12-22 14:26:43 +0800

    优化代码

--- library-expand/src/main/java/com/ooimi/expand/CollectionExpand.kt
+++ library-expand/src/main/java/com/ooimi/expand/CollectionExpand.kt
@@ -19,3 +19,21 @@ fun <T> List<T>.safeGet(index: Int): T? {
         null
     }
 }
+
+/**
+ * 安全获取List中的前面几个
+ */
+fun <T> List<T>.safeGetFront(size: Int): List<T> {
+    return try {
+        if (this.size > size) {
+            return this.subList(0, size - 1)
+        } else if (this.size == size) {
+            return this
+        } else {
+            return this.subList(0, this.size - 1)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        arrayListOf()
+    }
+}
--- library-expand/src/main/java/com/ooimi/expand/ConvertExpand.kt
+++ library-expand/src/main/java/com/ooimi/expand/ConvertExpand.kt
@@ -11,134 +11,121 @@ import android.util.TypedValue
 /**
  * int 转dp
  */
-fun Int.dp(): Int {
-    return TypedValue.applyDimension(
+val Int.dp: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转dp
  */
-fun Float.dp(): Float {
-    return TypedValue.applyDimension(
+val Float.dp: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
         this,
         Resources.getSystem().displayMetrics
     )
-}
 
 
 /**
  * int 转sp
  */
-fun Int.sp(): Int {
-    return TypedValue.applyDimension(
+val Int.sp: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_SP,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转sp
  */
-fun Float.sp(): Float {
-    return TypedValue.applyDimension(
+val Float.sp: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_SP,
         this,
         Resources.getSystem().displayMetrics
     )
-}
 
 /**
  * int 转px
  */
-fun Int.px(): Int {
-    return TypedValue.applyDimension(
+val Int.px: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_PX,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转px
  */
-fun Float.px(): Float {
-    return TypedValue.applyDimension(
+val Float.px: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_PX,
         this,
         Resources.getSystem().displayMetrics
     )
-}
 
 /**
  * int 转pt
  */
-fun Int.pt(): Int {
-    return TypedValue.applyDimension(
+val Int.pt: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_PT,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转pt
  */
-fun Float.pt(): Float {
-    return TypedValue.applyDimension(
+val Float.pt: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_PT,
         this,
         Resources.getSystem().displayMetrics
     )
-}
 
 /**
  * int 转in
  */
-fun Int.inn(): Int {
-    return TypedValue.applyDimension(
+val Int.inn: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_IN,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转in
  */
-fun Float.inn(): Float {
-    return TypedValue.applyDimension(
+val Float.inn: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_IN,
         this,
         Resources.getSystem().displayMetrics
     )
-}
-
 
 /**
  * int 转in
  */
-fun Int.mm(): Int {
-    return TypedValue.applyDimension(
+val Int.mm: Int
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_MM,
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
-}
 
 /**
  * Float 转in
  */
-fun Float.mm(): Float {
-    return TypedValue.applyDimension(
+val Float.mm: Float
+    get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_MM,
         this,
         Resources.getSystem().displayMetrics
     )
-}
 
